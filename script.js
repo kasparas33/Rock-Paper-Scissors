40496f69c02b158f08b467936a89a82b2467b1d6
Kasparas <273807439+kasparas33@users.noreply.github.com>
2026-06-15 16:02:46 +0300

    Add function for computer choice

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    const compChoice = Math.floor(Math.random() * 3)
+    if(compChoice === 0) {
+        return "rock"
+    } else if(compChoice === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
+
+// function getHumanChoice () 
+
+// const humanScore = ;
+// const computerScore = ;
+
+// function playRound()
